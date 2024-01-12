f1d948479f19b7e2469d070860ced2a47706910e
Alan Hernández <ahernandez102@ucol.mx>
2024-01-11 19:40:17 -0600

    Refactored gilded_rose.rb and added ruby standards to the left files
    Co-authored-by: Luis Gutierrez <luisgutierrez26489@gmail.com>
    Co-authored-by: Yhoselin Sánchez <yossi2075.ys@gmail.com>
    Co-authored-by: Fernando Arellano <farellano.0709@gmail.com>

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,58 +1,48 @@
-class GildedRose
+# frozen_string_literal: true
 
+# Clase GildedRose, recibe el objeto.
+class GildedRose
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def backstage_item(item)
+    puts item.quality
+    item.quality += 1 if item.sell_in > 10 && (item.quality < 50)
+    item.quality += 2 if (6..10).include?(item.sell_in) && item.quality < 50
+    item.quality += 3 if item.sell_in < 6 && (item.quality < 50)
+  end
+
+  def valid_dec_item(item)
+    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+      item.quality -= 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
+    elsif item.quality < 50
+      backstage_item(item) if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+    end
+  end
+
+  def first_zero_condition(item)
+    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+      item.quality -= 1 if item.quality.zero? && (item.name != 'Sulfuras, Hand of Ragnaros')
+    else
+      item.quality -= item.quality
+    end
+  end
+
+  def item_in_zero(item)
+    first_zero_condition(item) if item.sell_in.zero?
+  end
+
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      valid_dec_item(item)
+      item.sell_in -= 1 if item.name != 'Sulfuras, Hand of Ragnaros'
+      item_in_zero(item)
     end
   end
 end
 
+# Clase Item
 class Item
   attr_accessor :name, :sell_in, :quality
 
@@ -62,7 +52,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
--- ruby/gilded_rose_tests.rb
+++ ruby/gilded_rose_tests.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 require 'test/unit'
 
+# Inicializando prueba
 class TestUntitled < Test::Unit::TestCase
-
   def test_foo
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    assert_equal items[0].name, "fixme"
+    items = [Item.new('foo', 0, 0)]
+    GildedRose.new(items).update_quality
+    assert_equal items[0].name, 'fixme'
   end
-
 end
